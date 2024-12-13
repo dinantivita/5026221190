e02ee19d5b0945d0b126ac44afa7cc5e5b30b051
dinantivita <dinantvr@gmail.com>
2024-12-13 17:54:17 +0700

    latihan eas fix

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -26,6 +26,9 @@
             <li class="nav-item">
               <a class="nav-link" href="#">EAS</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/keranjang">Latihan EAS</a>
+              </li>
           </ul>
         </div>
 
